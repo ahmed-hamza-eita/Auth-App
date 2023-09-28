021d44dc069fbaf0650546327d3992d49bd15a37
hamza <aheita3@gmail.com>
2023-09-28 23:02:16 +0300

    mobile number screen

---
+++ app/src/main/java/com/hamza/authapp/ui/signup/AuthWithPhoneFragment.kt
@@ -0,0 +1,47 @@
+package com.hamza.authapp.ui.signup;
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hamza.authapp.R
+import com.hamza.authapp.databinding.AuthWithPhoneFragmentBinding
+import com.hamza.authapp.utils.BaseFragment
+
+
+class AuthWithPhoneFragment : BaseFragment() {
+
+    private var _binding: AuthWithPhoneFragmentBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        /*
+_binding = WallpaperFragmentBinding.inflate(layoutInflater, container, false)
+        return _binding?.root
+*/
+        return inflater.inflate(R.layout.auth_with_phone_fragment, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = AuthWithPhoneFragmentBinding.bind(view)
+
+actions()
+    }
+
+    private fun actions() {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
