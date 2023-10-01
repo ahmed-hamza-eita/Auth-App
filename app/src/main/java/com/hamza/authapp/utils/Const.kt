3b12f0b047b4fd812e2ab97de0743c9fa006ecc9
hamza <aheita3@gmail.com>
2023-10-01 07:48:01 +0300

    Sign in using mobile number

--- app/src/main/java/com/hamza/authapp/utils/Const.kt
+++ app/src/main/java/com/hamza/authapp/utils/Const.kt
@@ -5,12 +5,10 @@ import java.util.regex.Pattern
 class Const {
     companion object {
         const val KEY_IS_SIGNED_IN = "signed"
-        const val CLIENT_ID = "client id"
-        const val LOGIN_SUCCESS = "Login Success"
-        const val RESET_PASSWORD = "Reset Password"
+
         const val SIGNUP_SUCCESS_WITH_GOOGLE = "SignUp  with google account success"
         const val SIGNUP_SUCCESS = "SignUp success"
-        const val SIGNUP_SUCCESS_WITH_PHONE = "SignUp  with mobile number success"
+
         const val LOGOUT = "Signed out"
         const val CHECK_EMAIL = "Check your email address"
          const val GOOGLE_ACCOUNT_REQUEST = 10
